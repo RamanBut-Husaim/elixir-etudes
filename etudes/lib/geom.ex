b9e141f474b7703b806d6381f3c3e5a795cdff55
Raman But-Husaim <raman_but-husaim@live.com>
2017-05-14 14:24:24 +0300

    Etude 2.3

--- etudes/lib/geom.ex
+++ etudes/lib/geom.ex
@@ -1,6 +1,14 @@
 defmodule Geom do
-  @moduledoc false
+  @moduledoc """
+  Module with functions to calculate area for a geometric shape.
+  """
 
+  @doc """
+  Calculates the area of a rectangle, given the length and width.
+  Returns the product of its arguments. The default value for
+  both arguments is 1.
+  """
+  @spec area(number(), number()) :: number()
   def area(length \\ 1, width \\ 1) when is_number(length) and is_number(width) do
     length * width
   end
