86e006d8083e9351f36952eef8aaacf19927c6af
Raman But-Husaim <raman_but-husaim@live.com>
2017-05-20 19:12:34 +0300

    Etude 4.4

--- etudes/lib/powers.ex
+++ etudes/lib/powers.ex
@@ -1,20 +1,24 @@
 defmodule Powers do
-  import Kernel, except: [raise: 2]
+  import Kernel, except: [raise: 2, raise: 3]
   @moduledoc false
 
   def raise(_x, 0) do
     1
   end
 
-  def raise(x, 1) do
-    x
-  end
-
-  def raise(x, n) when n > 0 do
-    x * raise(x, n - 1)
-  end
-
   def raise(x, n) when n < 0 do
-    1.0 / raise(x, (-1) * n)
+    1.0 / raise(x, -n)
+  end
+
+  def raise(x, n) do
+    raise(x, n, 1)
+  end
+
+  defp raise(_x, n, accumulator) when n == 0 do
+    accumulator
+  end
+
+  defp raise(x, n, accumulator) do
+    raise(x, n - 1, accumulator * x)
   end
 end
